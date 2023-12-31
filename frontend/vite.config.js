669f0e59e4f99cd3c3c93f71356c04169a099b89
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-31 08:20:50 +0530

    changed config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 4000,
-    proxy: {
-      "/api": {
-        target: "https://mern-auth-backend-blond.vercel.app",
-        changeOrigin: true,
-      },
-    },
-  },
+  
 });
