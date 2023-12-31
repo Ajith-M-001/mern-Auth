4f6499f98326e20fdc03380b0a7d2596c70eb3c5
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-31 08:27:09 +0530

    chaded

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,14 @@ const PORT = process.env.PORT || 6000;
 
 // Define multiple options for CORS
 const corsOptions = {
-  origin: ['https://example.com', 'https://another-domain.com'], // Array of allowed origins
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
-  allowedHeaders: 'Content-Type,Authorization', // Allowed headers
+  origin: [
+    "https://mern-auth-frontend-af2i1ztu1-ajiths-projects-79e2278f.vercel.app",
+  ], // Array of allowed origins
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type,Authorization", // Allowed headers
   credentials: true, // Allow cookies and credentials
   preflightContinue: false, // Pass the CORS preflight response to the next handler
-  optionsSuccessStatus: 204 // Set the response status for preflight requests
+  optionsSuccessStatus: 204, // Set the response status for preflight requests
 };
 
 // Enable CORS with options
