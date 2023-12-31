3eb2d146944e49588fa281881440579814dc766b
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-31 07:52:29 +0530

    added vercel online api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://mern-auth-backend-blond.vercel.app",
         changeOrigin: true,
       },
     },
