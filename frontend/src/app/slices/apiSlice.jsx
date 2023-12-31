067a1a9eedd70c644c86ee0e9a15fdbfb795e8d4
Ajith-M-001 <ajithguru741@gmail.com>
2023-12-31 08:04:16 +0530

    added online api

--- frontend/src/app/slices/apiSlice.jsx
+++ frontend/src/app/slices/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "",
+  baseUrl: "https://mern-auth-backend-blond.vercel.app",
 });
 
 export const apiSlice = createApi({
