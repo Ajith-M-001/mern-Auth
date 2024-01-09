8c821471bd97d1568299fef1ab9288be41735d5f
Ajith-M-001 <ajithguru741@gmail.com>
2024-01-09 08:17:49 +0530

    added dist to backend

--- backend/index.js
+++ backend/index.js
@@ -22,11 +22,11 @@ app.use("/api/users", userRouter);
 
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  // const __dirname = path.resolve();
+  // app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
