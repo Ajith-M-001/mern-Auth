7e05442e72622c4b3c36a76e7302e5a4be13fc5d
Ajith-M-001 <ajithguru741@gmail.com>
2024-01-22 12:23:17 +0530

    changed user model

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -5,11 +5,14 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
+      minlength: 3,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
     },
     password: {
       type: String,
